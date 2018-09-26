7183d613f85e9810e873ef710435e8a9577e8e6c
dakolas <asegurog@alumnos.unex.es>
2018-09-26 19:38:52 +0200

    Add files via upload

---
+++ aseguro/choca/src/CMakeFiles/choca_autogen.dir/AutogenInfo.cmake
@@ -0,0 +1,39 @@
+# Meta
+set(AM_MULTI_CONFIG "SINGLE")
+# Directories and files
+set(AM_CMAKE_BINARY_DIR "/home/salabeta/robocomp/components/aseguro/choca/")
+set(AM_CMAKE_SOURCE_DIR "/home/salabeta/robocomp/components/aseguro/choca/")
+set(AM_CMAKE_CURRENT_SOURCE_DIR "/home/salabeta/robocomp/components/aseguro/choca/src/")
+set(AM_CMAKE_CURRENT_BINARY_DIR "/home/salabeta/robocomp/components/aseguro/choca/src/")
+set(AM_CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE "")
+set(AM_BUILD_DIR "/home/salabeta/robocomp/components/aseguro/choca/src/choca_autogen")
+set(AM_SOURCES "/home/salabeta/robocomp/classes/qlog/qlog.cpp;/home/salabeta/robocomp/classes/rapplication/rapplication.cpp;/home/salabeta/robocomp/classes/sigwatch/sigwatch.cpp;/home/salabeta/robocomp/components/aseguro/choca/src/commonbehaviorI.cpp;/home/salabeta/robocomp/components/aseguro/choca/src/genericmonitor.cpp;/home/salabeta/robocomp/components/aseguro/choca/src/genericworker.cpp;/home/salabeta/robocomp/components/aseguro/choca/src/main.cpp;/home/salabeta/robocomp/components/aseguro/choca/src/specificmonitor.cpp;/home/salabeta/robocomp/components/aseguro/choca/src/specificworker.cpp")
+set(AM_HEADERS "")
+# Qt environment
+set(AM_QT_VERSION_MAJOR "4")
+set(AM_QT_VERSION_MINOR "8")
+set(AM_QT_MOC_EXECUTABLE "/usr/lib/x86_64-linux-gnu/qt4/bin/moc")
+set(AM_QT_UIC_EXECUTABLE )
+set(AM_QT_RCC_EXECUTABLE )
+# MOC settings
+set(AM_MOC_SKIP "/home/salabeta/robocomp/components/aseguro/choca/src/choca_autogen/mocs_compilation.cpp")
+set(AM_MOC_DEFINITIONS "QT_CORE_LIB;QT_DLL;QT_GUI_LIB;QT_NO_DEBUG;QT_OPENGL_LIB;QT_XML_LIB")
+set(AM_MOC_INCLUDES "/home/salabeta/robocomp/components/aseguro/choca/src/choca_autogen/include;/usr/include/qt4;/usr/include/qt4/QtOpenGL;/usr/include/qt4/QtGui;/usr/include/qt4/QtXml;/usr/include/qt4/QtCore;/home/salabeta/robocomp/components/aseguro/choca/src;/home/salabeta/robocomp/components/aseguro/choca/src/.;/opt/robocomp/include;/classes;/home/salabeta/robocomp/components/aseguro/choca;/home/salabeta/robocomp/classes;/home/salabeta/robocomp/libs;/home/salabeta/robocomp/interfaces;/include")
+set(AM_MOC_OPTIONS "")
+set(AM_MOC_RELAXED_MODE "FALSE")
+set(AM_MOC_MACRO_NAMES "Q_OBJECT;Q_GADGET;Q_NAMESPACE")
+set(AM_MOC_DEPEND_FILTERS "")
+set(AM_MOC_PREDEFS_CMD )
+# UIC settings
+set(AM_UIC_SKIP )
+set(AM_UIC_TARGET_OPTIONS )
+set(AM_UIC_OPTIONS_FILES )
+set(AM_UIC_OPTIONS_OPTIONS )
+set(AM_UIC_SEARCH_PATHS )
+# RCC settings
+set(AM_RCC_SOURCES )
+set(AM_RCC_BUILDS )
+set(AM_RCC_OPTIONS )
+set(AM_RCC_INPUTS )
+# Configurations options
+set(AM_CONFIG_SUFFIX_ "_")
